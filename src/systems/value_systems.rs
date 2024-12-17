eec7f51c48672d80f701d1d66196b850bf0a5030
skyholeBishop <51252247+bigLRRH@users.noreply.github.com>
2024-12-17 20:21:40 +0800

    给value的系统增加一些.0

--- src/systems/value_systems.rs
+++ src/systems/value_systems.rs
@@ -3,8 +3,8 @@ use bevy::prelude::*;
 fn apply_value_updated(mut events: EventReader<ValueUpdated>, mut query: Query<(&mut Value, &Id)>) {
     for event in events.read() {
         for (mut value, id) in &query {
-            if id == event.target_id {
-                value = event.new_value;
+            if id.0 == event.target_id {
+                value.0 = event.new_value;
                 break;
             }
         }
@@ -18,10 +18,10 @@ fn apply_value_increased(
 ) {
     for event in events.read() {
         for (mut value, id) in &query {
-            if id == event.target_id {
+            if id.0 == event.target_id {
                 event_writer.send(ValueUpdated {
-                    target_id: id,
-                    new_value: value + event.increase_value,
+                    target_id: id.0,
+                    new_value: value.0 + event.increase_value,
                 });
                 break;
             }
@@ -36,10 +36,10 @@ fn apply_value_decreased(
 ) {
     for event in events.read() {
         for (mut value, id) in &query {
-            if id == event.target_id {
+            if id.0 == event.target_id {
                 event_writer.send(ValueUpdated {
-                    target_id: id,
-                    new_value: value - event.decrease_value,
+                    target_id: id.0,
+                    new_value: value.0 - event.decrease_value,
                 });
                 break;
             }
